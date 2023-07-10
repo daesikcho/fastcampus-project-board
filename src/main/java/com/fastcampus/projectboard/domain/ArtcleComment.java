936929da04db0c900fabfaa17222c42f1137ebb7
daesikCho <ses20002@google.com>
2023-07-10 14:32:51 +0900

    #8 도메인 클래스 설계 (게시글,댓글) 수정
    주석추가

--- src/main/java/com/fastcampus/projectboard/domain/ArtcleComment.java
+++ src/main/java/com/fastcampus/projectboard/domain/ArtcleComment.java
@@ -4,11 +4,11 @@ import java.time.LocalDateTime;
 
 public class ArtcleComment {
     private Long id;
-    private Article article;
-    private String content;
+    private Article article;   //게시글 (ID)
+    private String content;   //본문
 
-    private LocalDateTime createAt;
-    private String createBy;
-    private LocalDateTime modifyAt;
-    private String modifyBy;
+    private LocalDateTime createAt;  //생성일
+    private String createBy;  //생성자
+    private LocalDateTime modifyAt;   //수정자
+    private String modifyBy; //수정일
 }
--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -4,14 +4,14 @@ import java.time.LocalDateTime;
 
 public class Article {
     private Long id;
-    private String title;
-    private String content;
-    private String hashtag;
+    private String title;  // 제목
+    private String content; // 본문
+    private String hashtag; // 해시태그
 
-    private LocalDateTime createAt;
-    private String createBy;
-    private LocalDateTime modifyAt;
-    private String modifyBy;
+    private LocalDateTime createAt;  // 생성일
+    private String createBy;  //생성자
+    private LocalDateTime modifyAt;  // 수정일
+    private String modifyBy; // 수정자
 
 
 }
